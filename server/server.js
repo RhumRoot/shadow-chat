c295875b61d18813ccf1f95ad22c56d953daa634
Rhum <rhumroot@gmail.com>
2018-07-17 15:31:57 +0300

    testing start

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const config = require('./config.json')
 const PORT = process.env.PORT || 5000
 const event = new (require('events'))
 
-const Telegraf = require('telegraf')
+/* const Telegraf = require('telegraf')
 const express = require('express')
 const expressApp = express()
 
@@ -24,27 +24,25 @@ bot.command('start', (ctx) => ctx.reply('Hello rhum'))
 
 expressApp.listen(PORT, () => {
   console.log('Example app listening on port 3000!')
-})
+}) */
 
 
 
-/* 
+
 //App&Bot init
-const app = new (require('express'))
-const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
 
 const Telegraf = require('telegraf');
-const tg = new Telegraf(config.BOT_TOKEN)
+const tg = new Telegraf('657787507:AAFhAPCFmtWe9hHh2NEhf8_XFqx5ujzRq8M')
 
 const db = new (require('./db/client'))
 
 
 //App&Bot configuring
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 app.use(tg.webhookCallback('/telegram'))
 
-tg.telegram.setWebhook(config.URL + '/telegram').then(success => {
+tg.telegram.setWebhook('https://shadow-chat.herokuapp.com/telegram').then(success => {
     console.log(success)
 }, err => {
     console.log(`~~~ Error while building bot`)
@@ -56,11 +54,11 @@ tg.command('start', ctx => {
 
     console.log('start command for user: ', JSON.stringify(tgUser))
 
-    db.getUser(tgUser.id, user => {
+    /* db.getUser(tgUser.id, user => {
         !user && db.createUser(tgUser, user => {
             handler.start(user)
         })
-    }) 
+    })  */
 })
 
 //Bot launching
@@ -71,4 +69,3 @@ const bundle = { tg, db, event, config }
 
 
 app.listen(PORT, console.info(`Bot is working on ${PORT} port`))
- */
