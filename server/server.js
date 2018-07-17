61ff51dae6332e3248c3a335268384c3b6393766
Rhum <rhumroot@gmail.com>
2018-07-17 15:33:48 +0300

    testing start

--- server/server.js
+++ server/server.js
@@ -6,29 +6,6 @@ const config = require('./config.json')
 const PORT = process.env.PORT || 5000
 const event = new (require('events'))
 
-/* const Telegraf = require('telegraf')
-const express = require('express')
-const expressApp = express()
-
-
-const bot = new Telegraf('657787507:AAFhAPCFmtWe9hHh2NEhf8_XFqx5ujzRq8M')
-
-expressApp.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://shadow-chat.herokuapp.com/secret-path')
-
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-bot.command('start', (ctx) => ctx.reply('Hello rhum'))
-
-expressApp.listen(PORT, () => {
-  console.log('Example app listening on port 3000!')
-}) */
-
-
-
-
 //App&Bot init
 const express = require('express')
 const app = express()
@@ -41,7 +18,6 @@ const db = new (require('./db/client'))
 
 //App&Bot configuring
 app.use(tg.webhookCallback('/telegram'))
-
 tg.telegram.setWebhook('https://shadow-chat.herokuapp.com/telegram').then(success => {
     console.log(success)
 }, err => {
@@ -49,22 +25,22 @@ tg.telegram.setWebhook('https://shadow-chat.herokuapp.com/telegram').then(succes
     console.log(err)
 })
 
-tg.command('start', ctx => {
+/* tg.command('start', ctx => {
     let tgUser = ctx.message.from
 
     console.log('start command for user: ', JSON.stringify(tgUser))
 
-    /* db.getUser(tgUser.id, user => {
+    db.getUser(tgUser.id, user => {
         !user && db.createUser(tgUser, user => {
             handler.start(user)
         })
-    })  */
-})
+    }) 
+}) */
 
 //Bot launching
 const bundle = { tg, db, event, config }
 
-//const bot = new (require('../bot/bot.js'))(bundle)
+const bot = new (require('../bot/bot.js'))(bundle)
 
 
 
