bfc9b7a17408fe86a3d175c093866edf476eb8d5
Roman Koval <romariooo27@gmail.com>
2018-07-17 13:29:57 +0300

    method createUser

--- server/db/client.js
+++ server/db/client.js
@@ -15,7 +15,19 @@ class Client {
     }
 
     createUser(tgUser, cb) {
-        
+        let user =  new this.User()
+
+        user.id = tgUser.id
+        //user.isAdmin = false
+        user.username = tgUser.username
+        user.first_name = tgUser.first_name
+        user.last_name = tgUser.last_name
+        user.created = Date.now()
+        user.lastSeen = Date.now()
+
+        user.save((err, user) => {
+            cd( err ? undefined : user )
+        })
     }
 }
 
